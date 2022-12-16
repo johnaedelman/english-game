702d89de3a7078711ede1cce246714af17c267e4
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2022-12-15 23:52:08 -0800

    Update physics.py

--- lib/physics.py
+++ lib/physics.py
@@ -1,16 +1,21 @@
 
+
+vel_constant = 0.125  # The amount which velocity decays by
+
 def vel_check_decay(entity):  # Checks if the entity's velocity exceeds its maximum. If not, decays velocity so entity can come to a stop when moving
+
+    if entity.vel[0] > vel_constant:
+        entity.vel[0] -= vel_constant
+    if entity.vel[0] < -vel_constant:
+        entity.vel[0] += vel_constant
+    if -vel_constant <= entity.vel[0] <= vel_constant:
+        entity.vel[0] = 0
+
     for i in range(len(entity.vel)):  # Allows x and y vel to both be checked without explicitly stating entity.vel[0] and entity.vel[1]
-        if entity.vel[i] > entity.max_vel:
-            entity.vel[i] = entity.max_vel
-            continue  # Breaks from this iteration of the loop and continues to the next, meaning maximum velocities won't be decayed
         if entity.vel[i] < -entity.max_vel:
             entity.vel[i] = -entity.max_vel
-            continue
+    if entity.vel[0] > entity.max_vel:
+        entity.vel[0] = entity.max_vel
 
-        if entity.vel[i] > 0.075:
-            entity.vel[i] -= 0.075
-        if entity.vel[i] < -0.075:
-            entity.vel[i] += 0.075
-        if -0.075 < entity.vel[i] < 0.075:
-            entity.vel[i] = 0
+
+
