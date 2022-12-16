67422d17a2dd055609b733a288a0a3bd2f667d36
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2022-12-15 23:17:23 -0800

    More functional movement

--- game_main.py
+++ game_main.py
@@ -1,4 +1,6 @@
 import pygame
+from lib.physics import *
+from lib.entities import *
 import sys
 
 print("she call me hemingway when i frederic on her henry")
@@ -12,26 +14,39 @@ screen = pygame.display.set_mode(screen_size)
 main_clock = pygame.time.Clock()
 
 real = pygame.image.load("assets/sprites/quartz.png")
-real_pos = [(screen_size[0] - real.get_size()[0]) / 2, (screen_size[1] - real.get_size()[1]) / 2]
+quartz = Entity("quartz", [(screen_size[0] - real.get_size()[0]) / 2, (screen_size[1] - real.get_size()[1]) / 2], [0, 0], 5, real)
 
 while True:  # Begin the main loop
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # Checks if the game window has been closed. If so, stops the program
             sys.exit()
+
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_w]:
-        real_pos[1] -= 1
+    if keys[pygame.K_w]:  # Checks keypress for basic movement
+        quartz.vel[1] -= 1
     if keys[pygame.K_s]:
-        real_pos[1] += 1
+        quartz.vel[1] += 1
     if keys[pygame.K_d]:
-        real_pos[0] += 1
+        quartz.vel[0] += 1
     if keys[pygame.K_a]:
-        real_pos[0] -= 1
+        quartz.vel[0] -= 1
 
-    screen.blit(real, real_pos)
+    quartz.pos[0] += quartz.vel[0]
+    quartz.pos[1] += quartz.vel[1]
+
+    # Gravity (not working properly with the velocity check/decay at the moment, should be fixed)
+    """
+    if not quartz.pos[1] >= screen_size[1] - quartz.sprite.get_height():
+        quartz.vel[1] += 0.5
+    else:
+        quartz.vel[1] = 0
+        quartz.pos[1] = screen_size[1] - quartz.sprite.get_height()
+    """
+    vel_check_decay(quartz)
+
+    print(quartz.vel)
+    screen.blit(quartz.sprite, quartz.pos)
     pygame.display.update()
 
     main_clock.tick(60)  # Locks the game to run at 60 FPS
-
