32989b2c8b83cc068c6f6ff6f64cccaa364679a4
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2023-01-05 21:37:27 -0800

    Update entities.py

--- lib/entities.py
+++ lib/entities.py
@@ -6,3 +6,8 @@ class Entity:
         self.vel = vel  # Velocity
         self.max_vel = max_vel  # Velocity cap
         self.sprite = sprite
+
+
+class Player(Entity):
+    def __init(self, name, pos, vel, max_vel, sprite):
+        super().__init__(name, pos, vel, max_vel, sprite)
