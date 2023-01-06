016c1d6231242852a5b4fd29129ffcd59b82aea2
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2023-01-06 10:25:42 -0800

    Update entities.py

--- lib/entities.py
+++ lib/entities.py
@@ -1,13 +1,29 @@
+from lib.base import *
+
 
 class Entity:
-    def __init__(self, name, pos, vel, max_vel, sprite):
+    def __init__(self, name, **kwargs):
         self.name = name
-        self.pos = pos  # Position
-        self.vel = vel  # Velocity
-        self.max_vel = max_vel  # Velocity cap
-        self.sprite = sprite
+        self.sprite = pygame.image.load("assets/sprites/default_sprite.png")
+        self.pos = [(screen_size[0] - self.sprite.get_size()[0]) / 2, (screen_size[1] - self.sprite.get_size()[1]) / 2]  # Position
+        self.vel = [0, 0]  # Velocity
+        self.max_vel = 5  # Velocity cap
+        self.facing = "LEFT"
+        self.animation = [self.sprite]
+        self.animation_index = 0
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def animate(self):
+        if self.animation_index > len(self.animation) - 1:
+            self.animation_index = 0
+        self.sprite = self.animation[self.animation_index]
+        if self.facing == "RIGHT":
+            self.sprite = pygame.transform.flip(self.sprite, True, False)
+        self.animation_index += 1
 
 
 class Player(Entity):
-    def __init(self, name, pos, vel, max_vel, sprite):
-        super().__init__(name, pos, vel, max_vel, sprite)
+    def __init(self, name):
+        super().__init__(name)
