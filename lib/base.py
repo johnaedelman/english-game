ce40e1704d535100df25e38f14a37434d295d0ee
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2023-01-12 00:42:23 -0800

    Update base.py

--- lib/base.py
+++ lib/base.py
@@ -3,6 +3,12 @@ import pygame
 screen_size = (1280, 720)
 
 terrain_floor = [
-    [0, 540, 200],  # Lower bound on x-axis, upper bound on x-axis, y-value to set as floor
-    [541, 1080, 400]
+    [0, 540, 190],  # Lower bound on x-axis, upper bound on x-axis, y-value to set as floor
+    [541, 1080, 390]
+]
+
+terrain_lines = [
+    ((0, 520), (540, 520)),
+    ((540, 320), (1080, 320)),
+    ((540, 520), (540, 320))
 ]
