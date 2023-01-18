323fcc7eeb51104edd79692ae8c4f7fd4f7fa5e6
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2023-01-18 00:41:12 -0800

    Changed how animation loading works

--- lib/base.py
+++ lib/base.py
@@ -10,21 +10,41 @@ terrain_floor = [
 pygame.mixer.init()
 terrain = pygame.image.load("assets/sprites/terrain.png")
 marker = pygame.image.load("assets/sprites/marker.png")
+default_sprite = pygame.image.load("assets/sprites/default_sprite.png")
+
 healthbar_text = pygame.image.load("assets/sprites/healthbar_text.png")
 heart_empty = pygame.image.load("assets/sprites/heart_empty.png")
 heart_full = pygame.image.load("assets/sprites/heart_full.png")
+textbox = pygame.image.load("assets/sprites/textbox.png")
+
+alcohol = pygame.image.load("assets/sprites/alcohol.png")
 
 henry = pygame.image.load("assets/sprites/henry.png")
 henry_jump = pygame.image.load("assets/sprites/henry_jump.png")
+henry_jaundice = pygame.image.load("assets/sprites/henry_jaundice.png")
+henry_jump_jaundice = pygame.image.load("assets/sprites/henry_jump_jaundice.png")
 
-augh = pygame.mixer.Sound("assets/sounds/augh.mp3")
 boom = pygame.mixer.Sound("assets/sounds/vine_boom.mp3")
 riff = pygame.mixer.Sound("assets/sounds/riff.mp3")
 jump = pygame.mixer.Sound("assets/sounds/jump.mp3")
+drink = pygame.mixer.Sound("assets/sounds/drink.mp3")
 
 jump.set_volume(0.07)
 boom.set_volume(0.2)
 riff.set_volume(0.25)
+drink.set_volume(0.8)
 
 henry_default_animation = [henry]
 henry_jump_animation = [henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_jump, henry_default_animation]
+henry_default_animation_jaundice = [henry_jaundice]
+henry_jump_animation_jaundice = [henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_jump_jaundice, henry_default_animation_jaundice]
+
+henry_animations = {
+    "DEFAULT": henry_default_animation,
+    "JUMP": henry_jump_animation
+}
+
+henry_animations_jaundice = {
+    "DEFAULT": henry_default_animation_jaundice,
+    "JUMP": henry_jump_animation_jaundice
+}
