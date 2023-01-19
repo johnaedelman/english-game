d3f64811c1457a9e966b7e61ea2dcff3a207c0c1
johnaedelman <41525167+johnaedelman@users.noreply.github.com>
2023-01-18 17:38:53 -0800

    Update game_main.py

--- game_main.py
+++ game_main.py
@@ -24,15 +24,15 @@ powerup4 = Powerup("coffeebeans", powerup_type="COFFEEBEANS", pos=[400, 0])
 friendly_entity_1 = FriendlyEntity("Rinaldi", pos=[1500, 0], animation=[rinaldi])
 
 textbox_1 = Textbox("Rinaldi: ", "Mama Mia!! Frederic, baby! You look like death! You have contracted a terrible case of the disease known as jaundice. Here! Here my good man, have some delicious coffee beans, these are sure to get the alcohol out of your liver.", 5000, 30)
-textbox_2 = Textbox("Henry: ", "Thanks, Rinaldi. All of these Austrians are making me quiver. I am terrified! I hope Catherined is feeling well.", 4000, 30)
-textbox_3 = Textbox("Rinaldi: ", "Oh, I'm sure she's fine. No Austrian could capture old Miss Barkley. Good luck finding her baby!", 4000, 30)
+textbox_2 = Textbox("Henry: ", "Thanks, Rinaldi. All of these Austrians are making me quiver. I am terrified! I hope Catherine is feeling well.", 4000, 30)
+textbox_3 = Textbox("Rinaldi: ", "Oh, I'm sure she's fine. No Austrian could capture old Miss Barkley. Good luck finding her, baby!", 4000, 30)
 
 friendly_entity_1.conversation = [textbox_1, textbox_2, textbox_3]
 
 death_textbox = Textbox(None, "FAIL! You died! Rinaldi and Catherine will have to bury your skeleton into the earth in  a large hole they have dug with a shovel.  ", 4600, 46)
 current_textbox = None
 paused = False
-loaded_entities = [player, enemy, enemy2, enemy3, powerup, powerup2, powerup3, powerup4, friendly_entity_1]
+loaded_entities = [player, enemy, enemy2, enemy3, powerup, powerup2, powerup3, powerup4]
 frame_counter = 0
 
 while True:  # Begin the main loop
